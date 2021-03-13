d1e8cb8808bce3d5727f683c1cee8d4646910ead
Hugo Young <hybas3@gmail.com>
2021-03-13 17:12:22 +0000

    Zig

--- impls/zig/step0_repl.zig
+++ impls/zig/step0_repl.zig
@@ -1,23 +1,19 @@
 const std = @import("std");
 const warn = @import("std").debug.warn;
 
-const getline = @import("readline.zig").getline;
-
-const Allocator = @import("std").heap.c_allocator;
-
-fn READ(a: [] u8) [] u8 {
+fn READ(a: []u8) []u8 {
     return a;
 }
 
-fn EVAL(a: [] u8) [] u8 {
+fn EVAL(a: []u8) []u8 {
     return a;
 }
 
-fn PRINT(a: [] u8) [] u8 {
+fn PRINT(a: []u8) []u8 {
     return a;
 }
 
-fn rep(input: [] u8) [] u8 {
+fn rep(input: []u8) []u8 {
     var read_input = READ(input);
     var eval_input = EVAL(read_input);
     var print_input = PRINT(eval_input);
@@ -25,12 +21,17 @@ fn rep(input: [] u8) [] u8 {
 }
 
 pub fn main() !void {
-    const stdout_file = try std.io.getStdOut();
-    while(true) {
-        var line = (try getline(Allocator)) orelse break;
-        var output = rep(line);
-        try stdout_file.write(output);
-        Allocator.free(output);
-        try stdout_file.write("\n");
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+    while (true) {
+        var buf: [100]u8 = undefined;
+        _ = try stdout.write("user=> ");
+        if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
+            var output = rep(line);
+            _ = try stdout.write(output);
+            _ = try stdout.write("\n");
+        } else {
+            break;
+        }
     }
 }
